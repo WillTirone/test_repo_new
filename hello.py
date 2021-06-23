fad7366139047c3b3569e00995f822532ed6bad2
Will Tirone <will.tirone1@gmail.com>
2021-06-23 18:57:11 -0400

    made a new branch and added a class to our file

--- hello.py
+++ hello.py
@@ -2,3 +2,13 @@ print('hello class')
 
 def new_function(a,b):
     return a + b
+
+class Hello:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def hmm(c,d,e):
+        z = c + d + e 
+        return z
